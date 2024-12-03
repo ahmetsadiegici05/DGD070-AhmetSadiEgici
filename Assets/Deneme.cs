e5f9299854b29a9fd1123d5b3198a28fc098f016
ahmetegici <ahmetegici@gmail.com>
2024-12-03 19:29:41 +0300

    deneme

--- Assets/Deneme.cs
+++ Assets/Deneme.cs
@@ -7,12 +7,12 @@ public class Deneme : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("TestCube script çalýþýyor!");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(0, 50 * Time.deltaTime, 0);
     }
 }
